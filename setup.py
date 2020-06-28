3b5e570fd24d1794c03e681cc041c113af7f053b
Marco Bakera <marco@bakera.de>
2020-06-28 18:29:30 +0200

    added minimal requirements for webuntis.

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['webuntis'],
+    install_requires=['webuntis>=0.1.11'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
