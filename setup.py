965c523595a32a9d5a1047baf0f8e8324048bd4a
Marco Bakera <marco@bakera.de>
2020-06-28 18:27:32 +0200

    added mit license to the classifiers.

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
         'Topic :: Education',
 
         # Pick your license as you wish (should match "license" above)
-        # 'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
