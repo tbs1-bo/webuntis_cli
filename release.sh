80888bb88582aa3ec7060b53be86eaf86fc607d7
Marco Bakera <marco@bakera.de>
2020-06-28 18:32:41 +0200

    relying on python in venv

--- release.sh
+++ release.sh
@@ -4,8 +4,8 @@ echo "== cleaning dist folder"
 rm -v dist/*
 
 echo "== Creating distribution"
-python setup.py sdist
-python setup.py bdist_wheel
+venv/bin/python setup.py sdist
+venv/bin/python setup.py bdist_wheel
 
 echo "== Upload distribution"
 twine upload dist/*
